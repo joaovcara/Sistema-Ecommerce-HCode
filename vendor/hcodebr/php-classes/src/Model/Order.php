d2b28a462f07a43144b1b1460e81f0bb30d60e52
João Vitor de Oliveira Cara <62843342+joaovcara@users.noreply.github.com>
2021-03-03 17:17:06 -0300

    Pedidos Busca e Paginação

--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -133,6 +133,64 @@ class Order extends Model{
 
     }
 
+    public static function getPage($page = 1, $itemPerPage = 15){
+
+        $start = ($page - 1) * $itemPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT sql_calc_found_rows * 
+            FROM tb_orders a 
+            INNER JOIN tb_ordersstatus b USING(idstatus)
+            INNER JOIN tb_carts c USING(idcart)
+            INNER JOIN tb_users d ON d.iduser = a.iduser
+            INNER JOIN tb_addresses e USING(idaddress)
+            INNER JOIN tb_persons f ON f.idperson = d.idperson
+            ORDER BY a.dtregister DESC
+            LIMIT $start, $itemPerPage;
+        ");
+
+        $resultsTotal = $sql->select("SELECT found_rows() AS nrtotal");
+
+        return [
+            'data'=>$results,
+            'total'=>(int)$resultsTotal[0]['nrtotal'],
+            'pages'=>ceil($resultsTotal[0]['nrtotal'] / $itemPerPage)
+        ];
+
+    }
+
+    public static function getPageSearch($search, $page = 1, $itemPerPage = 15){
+
+        $start = ($page - 1) * $itemPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT sql_calc_found_rows * 
+            FROM tb_orders a 
+            INNER JOIN tb_ordersstatus b USING(idstatus)
+            INNER JOIN tb_carts c USING(idcart)
+            INNER JOIN tb_users d ON d.iduser = a.iduser
+            INNER JOIN tb_addresses e USING(idaddress)
+            INNER JOIN tb_persons f ON f.idperson = d.idperson
+            WHERE f.desperson LIKE :search 
+                OR b.desstatus LIKE :search
+            ORDER BY a.dtregister DESC
+            LIMIT $start, $itemPerPage;
+        ",[
+            ':search'=>'%'.$search.'%'
+        ]);
+
+        $resultsTotal = $sql->select("SELECT found_rows() AS nrtotal");
+
+        return [
+            'data'=>$results,
+            'total'=>(int)$resultsTotal[0]['nrtotal'],
+            'pages'=>ceil($resultsTotal[0]['nrtotal'] / $itemPerPage)
+        ];
+
+    }
+
 }
 
 ?>
