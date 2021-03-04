754e0e34d8ea3c0bdcd1661b86d085325c9a7558
João Vitor de Oliveira Cara <62843342+joaovcara@users.noreply.github.com>
2021-03-03 21:22:43 -0300

    Alterar senha admin

--- user.php
+++ user.php
@@ -5,6 +5,64 @@ use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
 #region ROTAS DE USUARIO
+$app->get("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-password", [
+		'user'=>$user->getValues(),
+		'msgError'=>User::getError(),
+		'msgSuccess'=>User::getSuccess()
+	]);
+
+});
+
+$app->post("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+
+	if(!isset($_POST['despassword']) || $_POST['despassword'] === ''){
+
+		User::setError("Preencha a nova senha");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	}
+
+	if(!isset($_POST['despassword-confirm']) || $_POST['despassword-confirm'] === ''){
+
+		User::setError("Confirme a senha.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+		
+	}
+
+	if($_POST['despassword'] !== $_POST['despassword-confirm']){
+
+		User::setError("As senhas não podem ser idênticas.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+		
+	}
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->setPassword(User::getPasswordHash($_POST['despassword']));
+
+	User::setSuccess("Senha alterada com sucesso.");
+	header("Location: /admin/users/$iduser/password");
+	exit;
+
+});
+
 $app->get('/admin/users', function(){
 
 	User::verifyLogin();
